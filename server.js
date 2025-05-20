6ce7bff537bb28db03988489adc81883a7277082
Shivam Singh <Shivam.singxh+git@gmail.com>
2025-05-20 19:36:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,21 +2,32 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
 app.use(express.static('public'));
 
+// Serve same HTML for any room URL
+app.get('/:room', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// In app.js or server.js
 io.on('connection', socket => {
-    console.log('A user connected');
+    socket.on('join room', room => {
+        socket.join(room);
+        console.log(`User joined room: ${room}`);
 
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg); // Broadcast to all users
-    });
+        socket.on('chat message', (msg) => {
+            const room = msg.room;
+            io.to(room).emit('chat message', msg); // Send full object back
+        });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+        });
     });
 });
 
 http.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
